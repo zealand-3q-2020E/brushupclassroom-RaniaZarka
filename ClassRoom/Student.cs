21c4e0458314d0d75bb9b8e6ddbc27c93b7b2695
Rania Zarka <rani0401@edu.easj.dk>
2020-08-26 12:50:03 +0200

    Task 1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Student
+    {
+        string name;
+        int birthMonth;
+        int birthDay;
+
+        public Student( string Name, int BirthMonth, int BirthDay)
+        {
+            name = Name;
+            birthDay = BirthDay;
+            birthMonth = BirthMonth;
+        }
+
+        public string Name { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthDay { get; set; }
+
+
+       
+    }
+}
